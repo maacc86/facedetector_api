b35f564151e1b8ebbf1a8660b79c0b9377c7e319
Markus Amon <markus.amon@gmx.net>
2020-10-20 20:48:22 +0200

    add env database

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-objective-33934',
-    user : 'postgres',
-    password : '1234',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
